0498cc5eb39b2c0c4fc829f37285060f0bf9f1ed
edogola4 <edogola4@gmail.com>
2025-04-25 11:29:19 +0300

    created next configuration file

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['placekitten.com'], // Placeholder domains for development
+    },
+    // Enable SWR for Progressive Web App capabilities
+   // swcMinify: true,
+  };
+  
+  module.exports = nextConfig;
+  
